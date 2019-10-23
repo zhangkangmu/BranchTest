d459603b60d51abea7b5314845be1c7195dfa100
zhangyuhong <289590351@qq.com>
2019-10-23 11:04:34 +0800

    补充增加跳转页面

--- app/src/main/java/com/study/hong/branchtest/MainActivity.java
+++ app/src/main/java/com/study/hong/branchtest/MainActivity.java
@@ -1,6 +1,7 @@
 package com.study.hong.branchtest;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -18,7 +19,7 @@ public class MainActivity extends Activity {
         onclick.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+startActivity(new Intent(MainActivity.this,ActivitySecond.class));
             }
         });
     }
