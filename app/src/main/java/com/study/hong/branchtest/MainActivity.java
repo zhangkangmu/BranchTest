01db7a5311117697325157994d87748274120c1c
zyh <289590351@qq.com>
2019-10-23 11:07:34 +0800

    增加测试依赖和application

--- app/src/main/java/com/study/hong/branchtest/MainActivity.java
+++ app/src/main/java/com/study/hong/branchtest/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        SKA.install(getApplication());
         onclick = findViewById(R.id.onclick);
         onclick.setOnClickListener(new View.OnClickListener() {
             @Override
---
+++ app/src/main/java/com/study/hong/branchtest/Myapplication.java
@@ -0,0 +1,15 @@
+package com.study.hong.branchtest;
+
+import android.app.Application;
+
+/**
+ * Created by hong on 2019/10/23.
+ */
+
+public class Myapplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
