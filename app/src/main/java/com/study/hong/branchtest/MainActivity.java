80c86c3c0180cf39184667f65853a9a2d207c76e
zyh <289590351@qq.com>
2019-10-23 12:10:08 +0800

    v1.0.3

--- app/src/main/java/com/study/hong/branchtest/MainActivity.java
+++ app/src/main/java/com/study/hong/branchtest/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -20,6 +21,7 @@ public class MainActivity extends Activity {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, ActivitySecond.class));
+                Log.e("zyh","增加第三个tag");
             }
         });
     }
